df5eb2c2ef5398ff43e62bff1e2f3727bf5af802
Parnassius <Parnassius@users.noreply.github.com>
2022-11-18 10:08:35 +0100

    Fix CI

--- offsets.py
+++ offsets.py
@@ -2,26 +2,26 @@ from itertools import product
 
 from PIL import Image
 
+def is_transparent(data):
+    return not any(x for x in data if x[3] != 0)
+
 offsets = []
 
 with Image.open("pokemonicons-sheet.png") as im:
-    transparency = im.info["transparency"]
-    empty_row = [transparency] * 40
-    empty_image = empty_row * 30
     for upper, left in product(range(0, im.height, 30), range(0, im.width, 40)):
         data = list(im.crop((left, upper, left + 40, upper + 30)).getdata())
 
-        if data == empty_image:
+        if is_transparent(data):
             offsets.append(0)
             continue
 
         upper_margin = 0
-        while data[upper_margin * 40 : (upper_margin + 1) * 40] == empty_row:
+        while is_transparent(data[upper_margin * 40 : (upper_margin + 1) * 40]):
             upper_margin += 1
 
         data.reverse()
         lower_margin = 0
-        while data[lower_margin * 40 : (lower_margin + 1) * 40] == empty_row:
+        while is_transparent(data[lower_margin * 40 : (lower_margin + 1) * 40]):
             lower_margin += 1
 
         sprite_height = 30 - lower_margin - upper_margin
